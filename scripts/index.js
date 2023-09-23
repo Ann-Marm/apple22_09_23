026bf86fc302bb5e0d9e13351c2ff9bc3a92fe15
Ann <Atl-anti-c8@yandex.ru>
2023-09-23 09:11:21 +0700

    simplebar, price

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
+/* simplebar  - скролл в выпадашке country 
+classNames - задаем свои классы скроллу */
+new SimpleBar(document.querySelector('.country__list'), {
+  classNames: {
+    scrollbar: 'country__scrollbar',
+    track: 'country__track'
+  }
+})
+
+
+
 /* полный слайдер - работает с ошибкой */
 /*import Swiper from '../lib/swiper-bundle.min.js';*/
 
@@ -41,7 +52,43 @@ new Swiper('.goods__block', {
     navigation: {
         prevEl: '.goods__arrow_prev',
         nextEl: '.goods__arrow_next'
+    },
+    preventClicks: true,
+    a11y: false,
+});
+
+
+/* modal */
+const productMore = document.querySelectorAll('.product__more');
+const modal = document.querySelector('.modal');
+
+productMore.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.classList.add('modal_open');
+  })
+})
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.classList.remove('modal_open');
+  }
+});
+
+
+/* когда кликаешь в инпут текст заготовка внутри -поднимается наверх */
+const formPlaceholder = document.querySelectorAll('.form__placeholder');
+const formInput = document.querySelectorAll('.form__input');
+
+formInput.forEach((input, i) => {
+  input.addEventListener('focus', () => {
+    formPlaceholder[i].classList.add('form__placeholder_active');
+  })
+
+  input.addEventListener('blur', () => {
+    if (input.value === '') {
+      formPlaceholder[i].classList.remove('form__placeholder_active')
     }
+  })
 });
 
 
@@ -88,3 +135,73 @@ const smothScroll = (trigger) => {
 
   smothScroll(document.querySelector('.header__navigation'));
   smothScroll(document.querySelector('.footer__navigation'));
+
+
+
+/*** price ***/
+
+const dataCurrency = {};
+
+const formatCurrency = (value, currency) => {
+return new Intl.NumberFormat('EUR', {
+  style: 'currency',
+  currency,
+  maximumFractionDigits: 2,
+}).format(value)
+}
+
+const showPrice = (currency = 'USD') => {
+  const priceElems = document.querySelectorAll('[data-price]');
+
+  priceElems.forEach(elem => {
+    elem.textContent = formatCurrency(elem.dataset.price * dataCurrency[currency], currency);
+  })
+}
+
+// showPrice(); 
+
+/* с сайта котировки валют скрипт, но переделанное */
+
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", "36xPzZYLB3Dd8bwRnXUlEU3rXoqFpqG5");
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+fetch("https://api.apilayer.com/fixer/latest?base=USD", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    //console.log('result', result);
+    Object.assign(dataCurrency, result.rates)
+    //console.log('dataCurrency', dataCurrency);
+    showPrice();
+  })
+  .catch(error => console.log('error', error));
+  
+     /*** price end  ***/
+
+
+      /* country choise */
+      
+  const coutnryBtn = document.querySelector('.country__btn');
+  const countryWrapper = document.querySelector('.country__wrapper');
+
+  coutnryBtn.addEventListener('click', () => {
+    countryWrapper.classList.toggle('country__wrapper_open');
+    })
+  
+
+    countryWrapper.addEventListener('click', ({target}) => {
+      if (target.classList.contains('country__choise')) {
+        countryWrapper.classList.remove('country__wrapper_open');
+        showPrice(target.dataset.currency);
+      }  
+      });
+
+
+
+    
